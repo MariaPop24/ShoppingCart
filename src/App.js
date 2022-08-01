bed77ae73776a32b6ef3f2b8066e258f4cb4eeec
MariaPop24 <popmaria461@yahoo.com>
2022-08-01 13:37:58 +0300

    added scss and refactored components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import ProductsList from "./components/ProductsList/ProductsList";
+import data from "./data";
 import "./App.css";
+import Cart from "./components/Cart/Cart";
 
-function App() {
+const App = () => {
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    const find = cartItems.find((item) => item.id === product.id)
+
+    if (find) {
+      setCartItems(cartItems.map((item) => item.id === product.id ? { ...find, quantity: find.quantity + 1 } : item))
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: 1 }])
+    }
+  }
+
+  const removeFromCart = (product) => {
+    const find = cartItems.find((item) => item.id === product.id);
+
+    if (find.quantity === 1) {
+      setCartItems(cartItems.filter((item) => item.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id ? { ...find, quantity: find.quantity - 1 } : item
+        )
+      );
+    }
+  }
   return (
     <div>
-      <Header />
-      <ProductsList />
+      <Header numberCartItems={cartItems.length} />
+      <div className="page-container">
+        <ProductsList products={products} addToCart={addToCart} />
+        <Cart
+          cartItems={cartItems}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart} />
+      </div>
+
     </div>
 
   );
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,45 @@
 import React from "react";
+import "./Cart.scss"
 
-const Cart = () => {
-	return <div></div>
+const Cart = (props) => {
+	const { cartItems, addToCart, removeFromCart } = props;
+	const totalPrice = cartItems.reduce((a, c) => a + c.price * c.quantity, 0);
+
+	return <div className="cart">
+		{cartItems.length === 0 &&
+			(<div className="empty-cart">
+				<div className="empty-cart-title">Cart is empty</div>
+				<div className="empty-cart-content">
+					Looks like you haven't added any prodicts to your cart yet.
+				</div>
+			</div>)}
+		<div>
+			{cartItems.length > 0 &&
+				cartItems.map((item) => {
+					<div key={item.id} className="row">
+						{console.log(item)}
+						<div className="col-2">{item.name}</div>
+						<div className="col-2">
+							<button onClick={() => removeFromCart(item)} className="remove">
+								-
+							</button>{' '}
+							<button onClick={() => addToCart(item)} className="add">
+								+
+							</button>
+						</div>
+
+						<div className="col-2 text-right">
+							{item.quantity} x ${item.price.toFixed(2)}
+						</div>
+						<div className="col-2">
+							<strong>Total Price: {totalPrice}</strong>
+						</div>
+					</div>
+				})}
+		</div>
+
+
+	</div>
 }
 
 export default Cart;
